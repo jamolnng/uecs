ca4866fbefd40559b080774b703d56f0a1817678
Jesse <guitarerman@gmail.com>
2019-11-27 00:39:06 -0600

    Update entity.hpp

--- include/uecs/entity.hpp
+++ include/uecs/entity.hpp
@@ -1,22 +1,24 @@
 #pragma once
 
-#include <bitset>
-#include <memory>
-#include <type_traits>
-#include <unordered_map>
+#include <iostream>
 
 #include <uecs/component.hpp>
 #include <uecs/config.hpp>
 #include <uecs/type.hpp>
 #include <uecs/unique_container.hpp>
+#include <uecs/util.hpp>
 
 namespace uecs {
 class Entity : public UniqueContainer<Component, MAX_ENTITY_COMPONENTS> {
  public:
-  Entity() : _id(0) {}
   const id_type id() const;
 
  private:
+  Entity(id_type id) : _id(id) {}
+  Entity(const Entity &) = delete;
+  Entity(Entity &&) = delete;
+  friend class EntityManager;
+  friend struct std::pair<const uecs::id_type, uecs::Entity>;
   id_type _id;
 };
 }  // namespace uecs
