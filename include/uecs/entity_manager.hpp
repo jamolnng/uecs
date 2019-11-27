d29a7b20fdb45b0d4ea1a4ee2fa55f0193e74aad
Jesse <guitarerman@gmail.com>
2019-11-27 00:38:57 -0600

    fix entity ids

--- include/uecs/entity_manager.hpp
+++ include/uecs/entity_manager.hpp
@@ -14,7 +14,7 @@ class EntityManager : NonCopyable {
   id_type _next_id{0};
   std::set<id_type> _unused_ids;
 
-  id_type create_id();
+  id_type reserve_id();
   void release_id(id_type id);
 };
 }  // namespace uecs
--- src/entity_manager.cpp
+++ src/entity_manager.cpp
@@ -1,21 +1,19 @@
-#include <iostream>
 #include <uecs/entity_manager.hpp>
 using namespace uecs;
 
-id_type EntityManager::create_id() {
-  id_type eid = TypeID<Entity, EntityManager>::value();
-  id_type id = eid;
+id_type EntityManager::reserve_id() {
+  id_type id;
   if (!_unused_ids.empty()) {
     auto b = _unused_ids.begin();
-    id |= (*b << (sizeof(id_type) * 8 / 2));
+    id = *b;
     _unused_ids.erase(b);
   } else {
-    id |= (_next_id << (sizeof(id_type) * 8 / 2));
-    _next_id++;
+    id = _next_id++;
   }
   return id;
 }
 
 void EntityManager::release_id(id_type id) {
   _unused_ids.insert(id >> (sizeof(id_type) * 8 / 2));
+void EntityManager::release_id(id_type id) { _unused_ids.insert(id); }
 }
