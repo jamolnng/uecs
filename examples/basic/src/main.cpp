9f765fa86636359a503c8926cbeb7f8746077954
Jesse <guitarerman@gmail.com>
2019-11-27 00:36:20 -0600

    add system manager test

--- examples/basic/src/main.cpp
+++ examples/basic/src/main.cpp
@@ -7,14 +7,20 @@
 class TestComponent : public uecs::Component {
  public:
   TestComponent(int i) : _i(i){};
-  ~TestComponent() { std::cout << "deleted" << std::endl; }
   int _i;
+};
+
+class TestSystem : public uecs::System {
+ public:
+  TestSystem() = default;
+  virtual void update(double dt) override { std::cout << dt << std::endl; }
 };
 
 int main() {
   std::cout << std::boolalpha;
 
-  uecs::Entity e;
+  uecs::EntityManager manager;
+  uecs::Entity& e = manager.create();
   std::cout << e.has<TestComponent>() << std::endl;
   auto tc = e.add<TestComponent>(99);
   std::cout << e.has<TestComponent>() << std::endl;
@@ -22,6 +28,17 @@ int main() {
   e.remove<TestComponent>();
   tc.reset(static_cast<TestComponent*>(nullptr));
   std::cout << e.has<TestComponent>() << std::endl;
+  manager.destroy(e.id());
+
+  uecs::SystemManager system(manager);
+
+  system.add<TestSystem>();
+
+  system.update<TestSystem>(manager, 10.0);
+  system.update(12);
+
+  system.remove<TestSystem>();
+  system.update(432);
 
   return 0;
 }
