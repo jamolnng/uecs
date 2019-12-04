ea7e58bc07a40be7e65d442bf6ce90c3b1d5bded
Jesse <guitarerman@gmail.com>
2019-12-04 00:04:57 -0600

    fixed sign compare

--- tests/test_entity_create_destroy.cpp
+++ tests/test_entity_create_destroy.cpp
@@ -11,16 +11,16 @@ REGISTER_TEST(test_entity_create_destroy) {
   UECS uecs;
   EntityManager& em = uecs._entities;
 
-  TASSERTD(em.size(), 0)
+  TASSERTD(em.size(), 0u)
   TASSERT(em.empty())
   Entity& e1 = em.create();
-  TASSERTD(em.size(), 1)
+  TASSERTD(em.size(), 1u)
   Entity& e2 = em.create();
-  TASSERTD(em.size(), 2)
+  TASSERTD(em.size(), 2u)
   em.destroy(e1);
-  TASSERTD(em.size(), 1)
+  TASSERTD(em.size(), 1u)
   em.destroy(e2);
-  TASSERTD(em.size(), 0)
+  TASSERTD(em.size(), 0u)
   TASSERT(em.empty())
 
   TPASS()
